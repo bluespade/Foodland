3843de520531ccbb39970d1f1daee959cf5c23e9
Lux Veritas <bshaner11@yahoo.com>
2018-07-05 02:44:57 -0400

    Overhauled attacking.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -34,6 +34,16 @@ public class Player : MonoBehaviour {
     private float attackTimer;
     private bool canAttack;
 
+    //Attack variables and references
+    private GameObject attackPoint;
+    private BoxCollider2D attackPointCollider;
+    private Vector2 attackPointOffset;
+    private Quaternion attackPointRotation;
+    private SpriteRenderer attackPointRenderer;
+    private float attackHitboxActiveTime = 0.2f;
+    private float attackTimer;
+    private bool canAttack;
+
     private int collisionCount = 0;
 
     private bool requestJump = false;
@@ -61,58 +71,56 @@ public class Player : MonoBehaviour {
             bool jumpButtonDownPressed = Input.GetButtonDown("Jump");
             requestJump = (requestJump || jumpButtonDownPressed) ? true : false;
 
-            float hor = Input.GetAxis("Horizontal");
-            float ver = Input.GetAxis("Vertical");
+        requestJump = (requestJump || jumpButtonDownPressed) ? true : false;
 
-            //Attack handling
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+
+        //Attack handling
+        if (hor > 0)
+        {
+            attackPointOffset = new Vector2(0.65f, 0f);
+            attackPointRotation = Quaternion.Euler(new Vector3(0f, 0f, 180f));
+        }
+        else
+        {
+            attackPointOffset = new Vector2(-0.65f, 0f);
+            attackPointRotation = Quaternion.Euler(new Vector3(0f, 0f, 0f));
+        }
+
+        if (ver > 0)
+        {
+            attackPointOffset = new Vector2(0f, 0.8f);
+            attackPointRotation = Quaternion.Euler(new Vector3(0f, 0f, 90f));
+        }
+
+        attackPoint.transform.localPosition = attackPointOffset;
+        attackPoint.transform.localRotation = attackPointRotation;
+
+        if (!canAttack)
+        {
+            attackTimer += Time.deltaTime;
+            //If the attack hitbox has been active for the specified amount of time...
+            if (attackTimer >= attackHitboxActiveTime)
+            {
+                //...then reset the timer to 0, reset attack point collider and renderer, and enable attacking.
+                canAttack = true;
+                attackTimer = 0f;
+
+                attackPointCollider.enabled = false;
+                attackPointRenderer.enabled = false;
+            }
+        }
+
+        //Get player attack input
+        if (Input.GetMouseButtonDown(0))
+        {
+            //If we can attack, show weapon and enable the attack hitbox
             if (canAttack)
             {
-                if (hor > 0)
-                {
-                    attackPointOffset = new Vector2(0.65f, 0f);
-                    attackPointRotation = Quaternion.Euler(new Vector3(0f, 0f, 180f));
-                }
-                else
-                {
-                    attackPointOffset = new Vector2(-0.65f, 0f);
-                    attackPointRotation = Quaternion.Euler(new Vector3(0f, 0f, 0f));
-                }
-
-                if (ver > 0)
-                {
-                    attackPointOffset = new Vector2(0f, 0.8f);
-                    attackPointRotation = Quaternion.Euler(new Vector3(0f, 0f, 90f));
-                }
-            }
-
-            attackPoint.transform.localPosition = attackPointOffset;
-            attackPoint.transform.localRotation = attackPointRotation;
-
-            if (!canAttack)
-            {
-                attackTimer += Time.deltaTime;
-                //If the attack hitbox has been active for the specified amount of time...
-                if (attackTimer >= attackHitboxActiveTime)
-                {
-                    //...then reset the timer to 0, reset attack point collider and renderer, and enable attacking.
-                    canAttack = true;
-                    attackTimer = 0f;
-
-                    attackPointCollider.enabled = false;
-                    attackPointRenderer.enabled = false;
-                }
-            }
-
-            //Get player attack input
-            if (Input.GetButtonDown("Fire1"))
-            {
-                //If we can attack, show weapon and enable the attack hitbox
-                if (canAttack)
-                {
-                    attackPointCollider.enabled = true;
-                    attackPointRenderer.enabled = true;
-                    canAttack = false;
-                }
+                attackPointCollider.enabled = true;
+                attackPointRenderer.enabled = true;
+                canAttack = false;
             }
         }
     }
