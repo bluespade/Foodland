3f124c3d7b4bfa34d8d2f00df7b38d6a6a0d5e45
Lux Veritas <bshaner11@yahoo.com>
2018-07-05 20:35:21 -0400

    Many things added.
    Added framework for item drops. Now using inheritance for individual enemy classes. Added a GameMaster object and class. Added health drop item, no functionality yet.

---
+++ Assets/Prefabs/Items/ItemDropController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDropController : MonoBehaviour {
+
+    Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+
+        rb.AddForce(new Vector2(Random.Range(-0.2f, 0.2f), Random.Range(2.2f, 2.4f)) * 150f);
+	}
+}
---
+++ Assets/Scripts/Enemies/AppleEnemy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleEnemy : Enemy {
+
+    public override void InitializeItemDrops()
+    {
+        base.InitializeItemDrops();
+
+        itemDrops.Add("HealthDrop_Small", 1.0f);
+    }
+}
