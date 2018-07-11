0cb6199ee2deb170c81ac58d95680c8b0ef5d684
snofz <snofz@umich.edu>
2018-07-10 18:32:50 -0700

    Added prefabs and scripts, meant to do in last commit

---
+++ Assets/Scripts/GameControl/Loader.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Loader : MonoBehaviour
+{
+    public GameObject gameMaster;
+
+
+    void Awake()
+    {
+        if (GameMaster.instance == null)
+        {
+            Instantiate(gameMaster);
+        }
+
+        gameMaster.GetComponent<GameMaster>().BeginGame(this.gameObject.scene.name, 0);
+    }
+}
